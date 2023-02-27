0ee35e568d58758d7e0ba4caabc8545213e94c43
Morten Høgsberg <morten.h.f.k@gmail.com>
2023-02-27 15:12:03 +0100

    finished parsers

--- nokia-ringtones-team-monad-master/Parser/Parser.fs
+++ nokia-ringtones-team-monad-master/Parser/Parser.fs
@@ -2,10 +2,130 @@
 
 open FParsec
 
+type Duration = float
 
-failwith "Implement here"
+type Note = 
+    | A 
+    | ASharp 
+    | B 
+    | C 
+    | CSharp 
+    | D 
+    | DSharp 
+    | E 
+    | F 
+    | FSharp 
+    | G 
+    | GSharp
 
-let pScore: Parser<Token list, unit> = // failwith "Implement here" // TODO 2 builder parser
+type Octave = int
+
+type Tone = {note: Note; octave: Octave}
+
+type RestTone = Rest 
+
+type Pitch = 
+    | Tone of Tone 
+    | Rest of RestTone
+
+type Token = {pitch: Pitch; duration: Duration}
+
+let noteToNumber note = 
+    match note with
+        | A -> 1
+        | ASharp -> 2
+        | B -> 3
+        | C -> 4
+        | CSharp -> 5
+        | D -> 6
+        | DSharp -> 7
+        | E -> 8
+        | F -> 9
+        | FSharp -> 10
+        | G -> 11
+        | GSharp -> 12
+    
+let durationParser: Parser<float, unit> =
+    let intParser: Parser<int, unit> = pint32
+    let trailingDotParser: Parser<char, unit> = pchar '.'
+    let multiplyBy1_5 (durationInt: int): float = float durationInt * 1.5
+    let convertToFloat (durationInt: int): float = float durationInt
+
+    let floatWithTrailingDotParser: Parser<float, unit> = 
+        (intParser |>> multiplyBy1_5) .>> followedBy trailingDotParser
+
+    let floatWithoutTrailingDotParser: Parser<float, unit> = 
+        (intParser |>> convertToFloat)
+
+    let durationParser': Parser<float, unit> = 
+        attempt floatWithTrailingDotParser <|> floatWithoutTrailingDotParser
+
+    durationParser'
+
+let noteParser: Parser<Note, unit> =
+    let noteParser': Parser<Note, unit> = 
+        attempt (pchar '#' >>. (pchar 'A') >>. preturn Note.ASharp) <|>
+        attempt (pchar 'A' >>. preturn Note.A) <|>
+        attempt (pchar 'B' >>. preturn Note.B) <|>
+        attempt (pchar '#' >>. (pchar 'C') >>. preturn Note.CSharp) <|>
+        attempt (pchar 'C' >>. preturn Note.C) <|>
+        attempt (pchar '#' >>. (pchar 'D') >>. preturn Note.DSharp) <|>
+        attempt (pchar 'D' >>. preturn Note.D) <|>
+        attempt (pchar 'E' >>. preturn Note.E) <|>
+        attempt (pchar '#' >>. (pchar 'F') >>. preturn Note.FSharp) <|>
+        attempt (pchar 'F' >>. preturn Note.F) <|>
+        attempt (pchar '#' >>. (pchar 'G') >>. preturn Note.GSharp) <|>
+        attempt (pchar 'G' >>. preturn Note.G)
+
+    noteParser'
+
+let octaveParser: Parser<Octave, unit> =
+    let intParser = pint32
+
+    intParser
+
+let toneParser: Parser<Tone, unit> = 
+    let noteParser: Parser<Note, unit> = noteParser
+    let octaveParser: Parser<Octave, unit> = octaveParser
+
+    let toneParser': Parser<Tone, unit> = 
+        noteParser .>>. octaveParser |>> (fun (note, octave) -> {note = note; octave = octave})
+
+    toneParser'
+
+let restParser: Parser<RestTone, unit> = 
+    let restParser': Parser<RestTone, unit> = 
+        pchar '-' >>. preturn RestTone.Rest
+
+    restParser'
+
+let pitchParser: Parser<Pitch, unit> = 
+    let toneParser: Parser<Tone, unit> = toneParser
+    let restParser: Parser<RestTone, unit> = restParser
+
+    let pitchParser': Parser<Pitch, unit> = 
+        attempt (toneParser |>> fun tone -> Pitch.Tone tone) <|>
+        attempt (restParser |>> fun rest -> Pitch.Rest rest)
+
+    pitchParser'
+
+let tokenParser: Parser<Token, unit> = 
+    let pitchParser: Parser<Pitch, unit> = pitchParser
+    let durationParser: Parser<float, unit> = durationParser
+
+    let tokenParser' =
+        pipe2 durationParser pitchParser (fun duration pitch -> {pitch = pitch; duration = duration})
+
+    let tokenParser'' = 
+        tokenParser' .>> spaces
+
+    tokenParser''
+    
+let pScore: Parser<Token list, unit> = 
+    let tokenParser: Parser<Token, unit> = tokenParser
+
+    many1 tokenParser
+    //failwith "Implement here" // TODO 2 builder parser
     
 
 let parse (input: string): Choice<string, Token list> =
@@ -29,16 +149,36 @@ let test (p: Parser<'a, unit>) (str: string): unit =
 // Eight note: 1/2 second
 // Sixteenth note 1/4 second
 // thirty-second note: 1/8
-let durationFromToken (token: Token): float = failwith "Implement here"
+let durationFromToken (token: Token): float =
+    let secondsPerBeat = 60.0 / 120.0
+    token.duration * secondsPerBeat * 1000.0
 
 // TODO 4 calculate overall index of octave
 // note index + (#octave-1) * 12
-let overallIndex (note, octave) = failwith "Implement here"
+let overallIndex (note, octave) = 
+    let octaveIndex = noteToNumber note + (octave - 1) * 12
+    octaveIndex
+    
+    // failwith "Implement here"
 
 // TODO 5 calculate semitones between to notes*octave
 // [A; A#; B; C; C#; D; D#; E; F; F#; G; G#]
 // overallIndex upper - overallIndex lower
-let semitonesBetween lower upper = failwith "Implement here"
+let semitonesBetween (lower: Tone) (upper: Tone): int = 
+    let offSets = [
+        A, 0; ASharp, 1; B, 2;
+        C, 3; CSharp, 4; D, 5;
+        DSharp, 6; E, 7; F, 8;
+        FSharp, 9; G, 10; GSharp, 11
+    ]
+    
+    let noteOffSetMap = Map.ofList offSets
+
+    let lowerOffset = noteOffSetMap.[lower.note]
+    let upperOffset = noteOffSetMap.[upper.note]
+
+    let semitones = (upper.octave - lower.octave) * 12 + (upperOffset - lowerOffset)
+    semitones
 
 // TODO 6
 // For a tone frequency formula can be found here: http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html
--- test/Program.fs
+++ test/Program.fs
@@ -1,61 +1,178 @@
 ﻿// import FParsec
 
 open FParsec
+open System.IO
+open System.Text
+open Microsoft.FSharp.Text
 
-// create a type of Duration that be either an integer or extended integer
 type Duration = float
 
-// create a parser that will parse Tone that can be a string of 
-// either "A", "#A", "B", "C", "#C", "D", "#D", "E", "F", "#F", "G", "#G"
-
 type Note = 
-    | A
-    | ASharp
-    | B
-    | C
-    | CSharp
-    | D
-    | DSharp
-    | E
-    | F
-    | FSharp
-    | G
-    | GSharp
-    | None
+    | A 
+    | ASharp 
+    | B 
+    | C 
+    | CSharp 
+    | D 
+    | DSharp 
+    | E 
+    | F 
+    | FSharp 
+    | G 
+    | GSharp 
 
 type Octave = int
 
+type Tone = {note: Note; octave: Octave} 
+type RestTone = Rest
+
 type Pitch = 
-    | Pitch of Note * Octave
-    | Rest
+    | Tone of Tone
+    | Rest of RestTone
 
-type Token = 
-    | Token of Pitch * Duration
+type Token = {pitch: Pitch; duration: Duration}
 
-// take a string and parse it to a Duration
+let durationParser: Parser<float, unit> =
+    let intParser: Parser<int, unit> = pint32
+    let trailingDotParser: Parser<char, unit> = pchar '.'
+    let multiplyBy1_5 (durationInt: int): float = float durationInt * 1.5
+    let convertToFloat (durationInt: int): float = float durationInt
 
-let durationParser (input: string): int =
-    let floatLiteral : Parser<int, unit> = pint32
-    let trailingDot : Parser<char, unit> = pchar '.'
-    let multiplyBy1_5 (f : int) : int = f + 2
+    let floatWithTrailingDotParser: Parser<float, unit> = 
+        (intParser |>> multiplyBy1_5) .>> trailingDotParser
 
-    let FloatWithDot = (floatLiteral |>> multiplyBy1_5) .>> followedBy (trailingDot) 
+    let floatWithoutTrailingDotParser: Parser<float, unit> = 
+        (intParser |>> convertToFloat)
 
-    let result = attempt FloatWithDot <|> floatLiteral
+    let durationParser': Parser<float, unit> = 
+        attempt floatWithTrailingDotParser <|> 
+        attempt floatWithoutTrailingDotParser
 
-    match run result input with
-    | Success (result, _, _) -> result
-    | Failure (errMsg, _, _) -> failwith errMsg
+    durationParser'
+
+let semitonesBetween (lower: Tone) (upper: Tone): int = 
+    let offSets = [ A, 0; ASharp, 1; B, 2; C, 3; CSharp, 4; D, 5; DSharp, 6; E, 7; F, 8; FSharp, 9; G, 10; GSharp, 11
+    ] 
+
+    let noteOffSetMap = Map.ofList offSets
+
+    let lowerOffSet = noteOffSetMap.[lower.note]
+    let upperOffSet = noteOffSetMap.[upper.note]
+
+    let semitones = (upper.octave - lower.octave + 1) * 12 + (upperOffSet - lowerOffSet) 
+
+    semitones
 
 
-let octaveParser (input: string): float = 
-    match run pfloat input with
-    | Success (result, _, _) -> result
-    | Failure (errMsg, _, _) -> failwith errMsg
+let noteParser: Parser<Note, unit> = 
+    // create a note parser that takes a string and returns a Note the strings are the note names
+
+    let noteParser': Parser<Note, unit> =
+        attempt (pchar '#' >>. (pchar 'A') >>. preturn Note.ASharp) <|>
+        attempt (pchar 'A' >>. preturn Note.A) <|>
+        attempt (pchar 'B' >>. preturn Note.B) <|>
+        attempt (pchar '#' >>. followedBy (pchar 'C') >>. preturn Note.CSharp) <|>
+        attempt (pchar 'C' >>. preturn Note.C) <|>
+        attempt (pchar '#' >>. followedBy (pchar 'D') >>. preturn Note.DSharp) <|>
+        attempt (pchar 'D' >>. preturn Note.D) <|>
+        attempt (pchar 'E' >>. preturn Note.E) <|>
+        attempt (pchar '#' >>. followedBy (pchar 'F') >>. preturn Note.FSharp) <|>
+        attempt (pchar 'F' >>. preturn Note.F) <|>
+        attempt (pchar '#' >>. followedBy (pchar 'G') >>. preturn Note.GSharp) <|>
+        attempt (pchar 'G' >>. preturn Note.G)
+
+    noteParser'
+
+let octaveParser: Parser<Octave, unit> = 
+    let intParser = pint32
+
+    intParser
 
 
-let result = durationParser "3"
-let result2 = durationParser "3."
+let toneParser: Parser<Tone, unit> = 
+    let noteParser = noteParser
+    let octaveParser = octaveParser
 
-printfn "%A" result
-printfn "%A" result2
+    let toneParser' = 
+        noteParser .>>. octaveParser |>> fun (note, octave) -> {note = note; octave = octave}
+
+    toneParser' 
+
+let restParser: Parser<RestTone, unit> =
+    let restParser' = 
+        pchar '-' >>. preturn RestTone.Rest
+
+    restParser'
+
+let pitchParser: Parser<Pitch, unit> = 
+    let toneParser = toneParser
+    let restParser = restParser
+
+    let pitchParser' = 
+        attempt (toneParser |>> fun tone -> Pitch.Tone tone) <|>
+        attempt (restParser |>> fun rest -> Pitch.Rest rest)
+
+    pitchParser'
+
+let tokenParser: Parser<Token, unit> =
+    let pitchParser = pitchParser
+    let durationParser = durationParser
+
+    let tokenParser' = 
+        pipe2 durationParser pitchParser (fun duration pitch -> {pitch = pitch; duration = duration})
+
+    let tokenParser'' = 
+        tokenParser' .>> spaces
+
+    tokenParser''
+
+
+
+let applyParser (input:string) (parser: Parser<'a, unit>) = 
+    match run parser input with
+    | Success(result, _, _) -> printfn "%A" result
+    | Failure(errorMsg, _, _) -> failwith errorMsg
+
+let applyTokenParser (input:string) = 
+    match run tokenParser input with
+    | Success(result, _, _) -> result
+    | Failure(errorMsg, _, _) -> failwith errorMsg
+
+applyParser "2" durationParser // expect 2.0
+applyParser "2." durationParser // expect 3.0
+
+applyParser "A" noteParser // expect A
+applyParser "#A" noteParser // expect ASharp
+
+applyParser "-" restParser // expect Rest
+
+applyParser "A2" pitchParser // expect Tone {note = A; octave = 2}
+applyParser "#A2" pitchParser // expect Tone {note = ASharp; octave = 2}
+
+applyParser "2A2" tokenParser // expect {pitch = Tone {note = A; octave = 2}; duration = 2.0}
+applyParser "2#A2" tokenParser // expect {pitch = Tone {note = ASharp; octave = 2}; duration = 2.0}
+applyParser "2-" tokenParser // expect {pitch = Rest RestTone.Rest; duration = 2.0}
+applyParser "2.-" tokenParser // expect {pitch = Rest RestTone.Rest; duration = 3.0}
+applyParser "2.A2" tokenParser // expect {pitch = Tone {note = A; octave = 2}; duration = 2.0}
+applyParser "2.#A2" tokenParser // expect {pitch = Tone {note = ASharp; octave = 2}; duration = 2.0}
+applyParser "2.E2" tokenParser 
+
+printfn "------------------"
+
+// apply tokenParser to a string of tokens 
+
+let result = "2.A2 2.#A2 2.E2"
+
+let resultList = result.Split(' ') |> Array.map (fun token -> applyTokenParser token)
+resultList |> Array.iter (fun token -> printfn "%A" token)
+
+printfn "------------------"
+
+let lowerTone = {note = C; octave = 0}
+let upperTone = {note = B; octave = 8}
+
+let semitones = semitonesBetween lowerTone upperTone
+
+printfn "%i" semitones
+
+applyParser result (many1 tokenParser)
